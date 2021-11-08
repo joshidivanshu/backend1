bb9d5dfccfa23a6b1e6137b2ed89d91d60dd6d39
Divanshu Joshi <joshidivanshu16@gmail.com>
2021-11-08 18:03:31 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wjhnk@c(**b3hwlwq(0u6wo8%0y(*8l^9k997e4kk_&(%32lzp'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-wjhnk@c(**b3hwlwq(0u6wo8%0y(*8l^9k997e4kk_&(%32lzp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
