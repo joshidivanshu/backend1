a0daf434cbb54b5f15a33bf9eff848130523fcdf
Divanshu Joshi <joshidivanshu16@gmail.com>
2021-11-08 17:51:31 +0530

    Initial commit

---
+++ app1/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.views import View
+from django.http.response import HttpResponse
+import json
+from rest_framework.views import APIView
+
+# Create your views here.
+
+
+class NameView(APIView):
+    def get(self, request):
+        name = request.GET.get('name','')
+        if not name:
+            return HttpResponse(json.dumps({'msg':'Name not provie'}))
+        return HttpResponse(json.dumps(("Happy Birthday, "+str(name)+"!")))
+
