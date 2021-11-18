e5ccd0b107a3a0bac486bc0be1f70c91ce4f0487
Divanshu Joshi <joshidivanshu16@gmail.com>
2021-11-18 22:22:47 +0530

    add: allowed host

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-wjhnk@c(**b3hw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['backend-1-zrdni.ondigitalocean.app']
+ALLOWED_HOSTS = ['backend-1-2-apw9j.ondigitalocean.app']
 
 
 # Application definition
